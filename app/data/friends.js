85acbf395b33f7a5fa05c9691f778f8571fce233
CLDavis15 <calebldavis0@gmail.com>
2019-04-22 16:46:17 -0400

    Added the friends array.

---
+++ app/data/friends.js
@@ -0,0 +1,37 @@
+
+
+var friendsArray = [
+    {
+        name: "Jon Snow",
+        photo: "https://www.telegraph.co.uk/content/dam/tv/2017/08/17/Jon-Snow-Beyond-the-Wall_trans_NvBQzQNjv4BqD3d2dmOlWYuQkR76XZjLQEvfX_p6SFMi1h6moBw3wqs.jpg",
+        score: [5, 4, 3, 2, 2, 1, 4, 5, 3, 2]
+
+    },
+    {
+        name: "Arya Stark",
+        photo: "https://vignette.wikia.nocookie.net/gameofthrones/images/b/b1/Arya_Season_8.jpg/revision/latest?cb=20190206213617",
+        score: [3, 4, 3, 2, 1, 1, 5, 5, 3, 4]
+     
+    },
+    {
+        name: "Daenerys Targaryen",
+        photo: "https://www.telegraph.co.uk/content/dam/fashion/2017/06/19/TELEMMGLPICT000131421802_trans_NvBQzQNjv4BqkUE_BTgBOQu3VWKvpDGX9fr7sARQy7EgBjwPUzvqL_M.jpeg?imwidth=450",
+        score: [5, 5, 2, 1, 2, 3, 4, 2, 3, 5]
+    },
+    {
+        name: "Sansa Stark",
+        photo: "https://upload.wikimedia.org/wikipedia/en/thumb/7/74/SophieTurnerasSansaStark.jpg/220px-SophieTurnerasSansaStark.jpg",
+        score: [2, 2, 1, 1, 3, 4, 5, 5, 2, 3]
+    },
+    {
+        name:"Tyrion Lannister",
+        photo: "https://static.toiimg.com/photo/62981209/.jpg",
+        score: [3, 4, 5, 3, 2, 4, 1, 5, 2, 1]
+    },
+    {
+        name: "Cersei Lannister",
+        photo: "https://upload.wikimedia.org/wikipedia/en/thumb/9/94/Cersei_Lannister-Lena_Headey.jpg/220px-Cersei_Lannister-Lena_Headey.jpg",
+        score: [4, 4, 2, 3, 2, 1, 5, 2, 5, 3]
+    }
+
+]
