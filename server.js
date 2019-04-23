318099f5dc13bf9220519c7121e2f5a59a28711e
CLDavis15 <calebldavis0@gmail.com>
2019-04-23 15:10:09 -0400

    Finishing touches to the html.

--- server.js
+++ server.js
@@ -7,12 +7,11 @@ var path = require("path");
 // =============================================================
 var app = express();
 var PORT = process.env.PORT || 3000;
+app.use(express.static("public"));
 
 // Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-
-
 
 
 
