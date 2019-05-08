f4d2556c461b6bcc4aa0a46503934b7b4808cd00
CLDavis15 <calebldavis0@gmail.com>
2019-05-08 16:55:50 -0400

    fixed modal.

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,42 +1,35 @@
-var path = require("path");
-
+var path = require('path');
 var friends = require("../data/friends.js");
 
 module.exports = function(app) {
-
-    app.get("/api/friends", function(req, res){
+    app.get("/api/friends", function(req,res){
         res.json(friends);
     });
 
-    app.post("/api/friends", function(req, res){ 
+    app.post("/api/friends", function(req, res){
         var userInput = req.body;
 
         var userResponse = userInput.scores;
 
-        var matchName = "";
-        var matchImage = "";
-        var totalDifference = 10000;
+        var matchName ="";
+        var matchImage ="";
+        var totalDiff = 10000;
 
-        for(var i = 0; i < friends.length; i++) {
+        for(var i = 0; i < friends.length; i++){
+             var diff = 0;
 
-            var diff = 0;
+             for(var i = 0; i < userResponse; i ++){
+                 diff += Math.abs(friends[i].scores[x])
+             }
 
-            for(var x = 0; x < userResponse; x++) {
-                diff += Math.abs(friends[i].scores[x] - userResponse[x]);
-            }
-
-            if(diff < totalDifference) {
-
-                totalDifference = diff;
-                matchName = friends[i].name;
-                matchImage = friends[i].photo;
-            }
+             if(diff < totalDiff){
+                 totalDiff = diff;
+                 matchName = friends[i].name;
+                 matchImage = friends[i].photo;
+             }
         }
-
-        // Add new user
         friends.push(userInput);
 
-        // Send response
         res.json({status: "Ok", matchName: matchName, matchImage: matchImage});
-    });
-};
+    })
+}
